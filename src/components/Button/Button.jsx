7b6945c09b4ab21055b517cfe4585f909abf7b6c
Emanuel Matias <matias.neto.edu@gmail.com>
2023-06-03 00:48:29 -0300

    finished

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import './Button.css';
+
+export default props => {
+    let classes = 'button ';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+    classes += props.operator ? 'operator' : '';
+    return (
+        <button
+            data-cy='btn'
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+        {props.label}
+        </button>
+    )
+}
---
+++ src/components/Display/Display.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import './Display.css';
+
+export default props => <div data-cy = 'display'className="display">{props.value}</div>
