f15d720c62fa32fc320f9cfc05af69eac5fd1c42
jhonnyferreiramendes <jhonnyferreiraczpb@gmail.com>
2023-06-02 21:51:05 -0300

    Primeiro commit

---
+++ src/main/cauculator.cy.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Calculator from './Calculator'
+
+describe('Teste do compoenente <Calculator />', () => {
+  beforeEach(() => {
+    cy.mount(<Calculator />)
+  })
+
+  it('Deve mostrar valor inicial ', () =>{
+    cy.get('[data-cy=display]').should('have.text', '0');
+  })
+
+  it('Deve adicionar digitos no display', () =>{
+    cy.get('[data-cy=btn]').contains('2').click()
+    cy.get('[data-cy=btn]').contains('0').click()
+    cy.get('[data-cy=btn]').contains('2').click()
+    cy.get('[data-cy=btn]').contains('3').click()
+    cy.get('[data-cy=display]').should('have.text','2023')
+
+  });
+
+  it('Deve realizar adição ', ()=> {
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("–").click()
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("=").click()
+    cy.get('[data-cy=display]').should('have.text','0')
+
+  });
+
+  it('Deve realizar adição ', ()=> {
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("+").click()
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("=").click()
+    cy.get('[data-cy=display]').should('have.text','10')
+
+  });
+
+  it('Deve realizar adição ', ()=> {
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("×").click()
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("=").click()
+    cy.get('[data-cy=display]').should('have.text','25')
+
+  });
+
+  it('Deve realizar adição ', ()=> {
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("÷").click()
+    cy.get('[data-cy=btn]').contains('5').click()
+    cy.get('[data-cy=btn]').contains("=").click()
+    cy.get('[data-cy=display]').should('have.text','1')
+});
+
+it('Deve realizar adição ', ()=> {
+  cy.get('[data-cy=btn]').contains('5').click()
+  cy.get('[data-cy=btn]').contains("÷").click()
+  cy.get('[data-cy=btn]').contains('0').click()
+  cy.get('[data-cy=btn]').contains("=").click()
+  cy.get('[data-cy=display]').should('have.text','Infinity')
+});
+
+});
+
